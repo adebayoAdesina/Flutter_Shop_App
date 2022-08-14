a5f9742771407604989114732b78b40de1608902
adebayoAdesina <adebayoadesina789@gmail.com>
2022-08-14 10:02:35 +0100

    form validation added to edit product Screen.

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,5 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/appdata.dart';
+import 'package:shop_app/providers/product_model.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const id = '/edit-product';
@@ -12,55 +15,141 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  FocusNode _imageUrlFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
+  final _formGlobal = GlobalKey<FormState>();
+  Product editProduct = Product();
 
   @override
   void dispose() {
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
     _imageUrlController.dispose();
+    // _imageUrlFocusNode.dispose();
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
     super.dispose();
   }
 
   @override
+  void initState() {
+    super.initState();
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    _imageUrlFocusNode = FocusNode();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
+  void saveForm() {
+    final isValid = _formGlobal.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    _formGlobal.currentState!.save();
+    print(isValid);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final productId = context.watch<AppData>().products.length;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Edit Product'),
-      ),
+      appBar: AppBar(title: const Text('Edit Product'), actions: [
+        IconButton(
+          onPressed: () => saveForm(),
+          icon: const Icon(Icons.save),
+        ),
+      ]),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Form(
+          key: _formGlobal,
           child: ListView(
             children: [
               TextFormField(
-                  decoration: const InputDecoration(
-                    label: Text('Title'),
+                decoration: InputDecoration(
+                  label: const Text('Title'),
+                  errorStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.error,
+                    fontSize: 12,
                   ),
-                  textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_priceFocusNode);
-                  }),
+                ),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  return value!.isEmpty ? 'Please input a Title.' : null;
+                },
+                onChanged: (value) {
+                  editProduct = Product(
+                    id: productId,
+                    title: value,
+                    price: editProduct.price,
+                    description: editProduct.description,
+                    imageUrl: editProduct.imageUrl,
+                  );
+                },
+              ),
               TextFormField(
-                  decoration: const InputDecoration(
-                    label: Text('Price'),
+                decoration: InputDecoration(
+                  label: const Text('Price'),
+                  errorStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.error,
+                    fontSize: 12,
                   ),
-                  keyboardType: TextInputType.number,
-                  textInputAction: TextInputAction.next,
-                  focusNode: _priceFocusNode,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_descriptionFocusNode);
-                  }),
+                ),
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.next,
+                focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+                validator: (value) {
+                  return value!.isEmpty ? 'Please input a Price.' : null;
+                },
+                onChanged: (value) {
+                  editProduct = Product(
+                    id: productId,
+                    title: editProduct.title,
+                    price: double.parse(value),
+                    description: editProduct.description,
+                    imageUrl: editProduct.imageUrl,
+                  );
+                },
+              ),
               TextFormField(
-                decoration: const InputDecoration(
-                  label: Text('Description'),
+                decoration: InputDecoration(
+                  label: const Text('Description'),
+                  errorStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.error,
+                    fontSize: 12,
+                  ),
                 ),
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 textInputAction: TextInputAction.next,
                 focusNode: _descriptionFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_imageUrlFocusNode);
+                },
+                validator: (value) {
+                  return value!.isEmpty ? 'Please input a Description.' : null;
+                },
+                onChanged: (value) {
+                  editProduct = Product(
+                    id: productId,
+                    title: editProduct.title,
+                    price: editProduct.price,
+                    description: value,
+                    imageUrl: editProduct.imageUrl,
+                  );
+                },
               ),
               Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Container(
                     margin: const EdgeInsets.only(top: 8, right: 10),
@@ -84,12 +173,32 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   ),
                   Expanded(
                     child: TextFormField(
-                      decoration: const InputDecoration(
-                        label: Text('Image Url'),
+                      decoration: InputDecoration(
+                        label: const Text('Image Url'),
+                        errorStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.error,
+                          fontSize: 12,
+                        ),
                       ),
                       keyboardType: TextInputType.url,
-                      textInputAction: TextInputAction.done,
                       controller: _imageUrlController,
+                      textInputAction: TextInputAction.done,
+                      focusNode: _imageUrlFocusNode,
+                      onFieldSubmitted: (_) => saveForm(),
+                      validator: (value) {
+                        return value!.isEmpty
+                            ? 'Please input a Image URL.'
+                            : null;
+                      },
+                      onChanged: (value) {
+                        editProduct = Product(
+                          id: productId,
+                          title: editProduct.title,
+                          price: editProduct.price,
+                          description: editProduct.description,
+                          imageUrl: value,
+                        );
+                      },
                     ),
                   ),
                 ],
