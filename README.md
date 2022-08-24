ece6a21cb301786d746a76c406c8338aa7be5f2a
adebayoAdesina <adebayoadesina789@gmail.com>
2022-08-24 11:48:24 +0100

    read me screenshot added.

--- README.md
+++ README.md
@@ -6,11 +6,10 @@ A new Flutter project.
 
 This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+<div style="display: grid; grid-column: 2;">
+    <img src="assets/readMeScreenshot/Screenshot_2022.08.24_11.42.33.767.png" width= 200>
+    <img src="assets/readMeScreenshot/Screenshot_2022.08.24_11.42.56.018.png" width= 200>
+    <img src="assets/readMeScreenshot/Screenshot_2022.08.24_11.43.07.635.png" width= 200>
+    <img src="assets/readMeScreenshot/Screenshot_2022.08.24_11.43.12.385.png" width= 200>
+    <img src="assets/readMeScreenshot/Screenshot_2022.08.24_11.43.16.317.png" width= 200>
+</div>
--- lib/providers/appdata.dart
+++ lib/providers/appdata.dart
@@ -15,46 +15,6 @@ class AppData with ChangeNotifier {
   AppData(this.authToken, this._products);
 
   List<Product> _products = [];
-  // Product(
-  //   id: '1',
-  //   title: 'Blue Shirt',
-  //   description: 'A blue shirt',
-  //   price: 30.00,
-  //   imageUrl:
-  //       'https://th.bing.com/th/id/OIP.5fRJB0DfiQQ0t1IR20g4WQHaKC?pid=ImgDet&rs=1',
-  // ),
-  // Product(
-  //   id: '2',
-  //   title: 'Black Shirt',
-  //   description: 'A black shirt',
-  //   price: 30.60,
-  //   imageUrl:
-  //       'https://media.missguided.com/i/missguided/ZX9221280_01?fmt=jpeg&fmt.jpeg.interlaced=true&\$product-page__main--3x\$',
-  // ),
-  // Product(
-  //   id: '3',
-  //   title: 'Red Shirt',
-  //   description: 'A Red shirt',
-  //   price: 58.80,
-  //   imageUrl:
-  //       'https://th.bing.com/th/id/R.a9ecb57dd4e13656eb01666e9666bf70?rik=KXDZJ7w1W%2b8mxg&pid=ImgRaw&r=0',
-  // ),
-  // Product(
-  //   id: '4',
-  //   title: 'Blue Shirt',
-  //   description: 'A blue shirt',
-  //   price: 35.60,
-  //   imageUrl:
-  //       'https://www.karmakula.co.uk/images/hawaiian/63d0afc173374f37a7eb1cd5e3c77456.jpg',
-  // ),
-  // Product(
-  //   id: '5',
-  //   title: 'Brown Shirt',
-  //   description: 'A Brown shirt',
-  //   price: 30.00,
-  //   imageUrl:
-  //       'https://th.bing.com/th/id/OIP.5fRJB0DfiQQ0t1IR20g4WQHaKC?pid=ImgDet&rs=1',
-  // ),
 
   Future<void> fetchProduct() async {
     String url =
